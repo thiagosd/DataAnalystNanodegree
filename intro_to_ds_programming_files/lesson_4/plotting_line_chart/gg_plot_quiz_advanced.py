583c7a9377c7dbe3e761aa1bc37e033eac036d8b
Thiago D <thiagosd@gmail.com>
2014-11-30 14:20:49 -0500

    lesson 4

--- intro_to_ds_programming_files/lesson_4/plotting_line_chart/gg_plot_quiz_advanced.py
+++ intro_to_ds_programming_files/lesson_4/plotting_line_chart/gg_plot_quiz_advanced.py
@@ -1,34 +1,39 @@
-import matplotlib
-matplotlib.use('agg')
+# import matplotlib
+#matplotlib.use('agg')
 
 import pandas as pd
 from ggplot import *
 
+
 def lineplot_compare(hr_by_team_year_sf_la_csv):
     # Write a function, lineplot_compare, that will read a csv file
-    # called hr_by_team_year_sf_la_csv and plot it using pandas and ggplot2.
+    # called hr_by_team_year_sf_la.csv and plot it using pandas and ggplot2.
     #
-    # This csv file has three columns -- yearID, HR, and teamID, 
-    # representing the total number of HR hit each year by the SF Giants 
-    # and LA Dodgers. Produce a visualization comparing the total HR by 
-    # year of the two teams. 
-    # 
+    # This csv file has three columns: yearID, HR, and teamID. The data in the
+    # file gives the total number of home runs hit each year by the SF Giants
+    # (teamID == 'SFN') and the LA Dodgers (teamID == "LAN"). Produce a
+    # visualization comparing the total home runs by year of the two teams.
+    #
     # You can see the data in hr_by_team_year_sf_la_csv
     # at the link below:
     # https://www.dropbox.com/s/wn43cngo2wdle2b/hr_by_team_year_sf_la.csv
     #
-    # Note that to differentiate between multiple categories on the 
+    # Note that to differentiate between multiple categories on the
     # same plot in ggplot, we can pass color in with the other arguments
-    # to aes, rather than in our geometry functions.
-    # 
-    # For example, ggplot(data, aes(xvar, yvar, color=category_var)).  This
-    # should help you.
-    
-    gg = #YOUR CODE GOES HERE
+    # to aes, rather than in our geometry functions. For example,
+    # ggplot(data, aes(xvar, yvar, color=category_var)). This should help you
+    # in this exercise.
+
+    hr_by_team_year_sf_la_df = pd.read_csv(hr_by_team_year_sf_la_csv)
+
+    gg = ggplot(hr_by_team_year_sf_la_df, aes('yearID', 'HR', color='teamID')) + geom_point() + geom_line() + ggtitle(
+        '# of Home Runs by Year') + xlab('Year') + ylab('# of Home Runs')
+
     return gg
+
 
 if __name__ == "__main__":
     data = "hr_by_team_year_sf_la.csv"
     image = "plot.png"
-    gg =  lineplot_compare(data)
+    gg = lineplot_compare(data)
     ggsave(image, gg, width=11, height=8)
--- intro_to_ds_programming_files/lesson_4/plotting_python/gg_plot_quiz_student.py
+++ intro_to_ds_programming_files/lesson_4/plotting_python/gg_plot_quiz_student.py
@@ -1,28 +1,41 @@
 import matplotlib
+
 matplotlib.use('agg')
 
 import pandas as pd
 from ggplot import *
 
+
 def lineplot(hr_year_csv):
-    # Assume that we have a pandas dataframe file called hr_year, 
-    # which contains two columns -- yearID, and HR.  
-    # 
-    # The pandas dataframe contains the number of HR hit in the
-    # Major League baseball in each year.  Can you write a function,
-    # lineplot, that creates a chart with points connected by lines, both
-    # colored 'red', showing the number of HR by year?
+    # A csv file will be passed in as an argument which
+    # contains two columns -- 'HR' (the number of homerun hits)
+    # and 'yearID' (the year in which the homeruns were hit).
     #
-    # You can check out the data loaded into the dataframe at the link below:
+    # Fill out the body of this function, lineplot, to use the
+    # passed-in csv file, hr_year.csv, and create a
+    # chart with points connected by lines, both colored 'red',
+    # showing the number of HR by year.
+    #
+    # You will want to first load the csv file into a pandas dataframe
+    # and use the pandas dataframe along with ggplot to create your visualization
+    #
+    # You can check out the data in the csv file at the link below:
     # https://www.dropbox.com/s/awgdal71hc1u06d/hr_year.csv
-    
+    #
+    # You can read more about ggplot at the following link:
+    # https://github.com/yhat/ggplot/
+
     # your code here
-    gg = # more of your code here
+    hr_year_df = pd.read_csv(hr_year_csv)
+
+    gg = ggplot(hr_year_df, aes('yearID', 'HR')) + geom_point(color='red') + geom_line(
+        color='red') + ggtitle('# of Home Runs by Year') + xlab('Year') + ylab('# of Home Runs')
+
     return gg
 
 
 if __name__ == "__main__":
     data = "hr_year.csv"
     image = "plot.png"
-    gg =  lineplot(data)
+    gg = lineplot(data)
     ggsave(image, gg, width=11, height=8)
