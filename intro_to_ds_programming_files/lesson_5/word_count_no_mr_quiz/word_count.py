195705b84f4a209659a61f1f4aaaad0e23ad538d
Thiago D <thiagosd@gmail.com>
2014-12-03 08:44:54 -0500

    Lesson 5

--- intro_to_ds_programming_files/lesson_5/word_count_no_mr_quiz/word_count.py
+++ intro_to_ds_programming_files/lesson_5/word_count_no_mr_quiz/word_count.py
@@ -1,5 +1,7 @@
 import sys
 import string
+import logging
+
 
 def word_count():
     # We are going to count the occurences of all the words that appear in the book
@@ -11,25 +13,40 @@ def word_count():
     # The text in Alice in Wonderland will be fed into this program line by line.
     # And you need to write a program that will take each line and do the following:
     # 1) Tokenize a line of text into string tokens, by white space
-    #    Example: "Hello, World!" will be converted into "Hello," and "World!"
+    # Example: "Hello, World!" will be converted into "Hello," and "World!"
     #
     # 2) Remove all punctuations
-    #    Example: "Hello," and "World!" will be converted to "Hello" and "World"
+    # Example: "Hello," and "World!" will be converted to "Hello" and "World"
     #
     # 3) Convert all words into lowercases
-    #    Example: "Hello" and "World" will be converted to "hello" and "world"
+    # Example: "Hello" and "World" will be converted to "hello" and "world"
     #
     # Store the the number of times that a word appears in Alice in Wonderland
     # in the word_counts dictionary
 
 
-    word_counts = {}
+    # os.system('word_count_no_mr.sh')
 
-    for line in sys.stdin:
+    word_counts = {}
+    text_file = open('aliceInWonderland.txt', "r")
+    lines = text_file.readlines()
+
+    for line in lines:
         data = line.strip().split(" ")
-        
+        for word in data:
+            clean_word = word.translate(string.maketrans("", ""), string.punctuation).lower()
+
+            if clean_word in word_counts:
+                word_counts[clean_word] += 1
+            else:
+                word_counts[clean_word] = 1
+
         # Your code here
 
     print word_counts
 
+
 word_count()
+
+
+# http://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string-in-python
