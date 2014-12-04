1a943b5663be88d4a2d470330046976120b06b66
Thiago D <thiagosd@gmail.com>
2014-12-04 00:40:42 -0500

    Lesson 5

--- intro_to_ds_programming_files/lesson_5/aadhaar_generated_quiz/aadhaar_generated_mapper.py
+++ intro_to_ds_programming_files/lesson_5/aadhaar_generated_quiz/aadhaar_generated_mapper.py
@@ -1,27 +1,49 @@
+import csv
 import sys
 import string
 
+
 def mapper():
-    
-    for line in sys.stdin: #cycle through lines of code
-        
-        #Your mapper code goes here.
-        #You will also need to fill out the reducer
-        #code as well before test running or else you will get an error.
-        #
-        #Each line will be
-        #a comma-separated list of values.  The
-        #header row WILL be included.  Tokenize
-        #each row using the commas, and emit a key-
-        #value pair containing the district and
-        #Aadhaar generated, separated by a tab.
-        #Make sure each row has the correct number
-        #of tokens and is not the header row!
-        #
-        #You can see a copy of the the input aadhaar data
-        #in the link below:
-        #https://www.dropbox.com/s/vn8t4uulbsfmalo/aadhaar_data.csv
-        
-        #your code here
-        
+    # Also make sure to fill out the reducer code before clicking "Test Run" or "Submit".
+
+    # Each line will be a comma-separated list of values. The
+    # header row WILL be included. Tokenize each row using the
+    #commas, and emit (i.e. print) a key-value pair containing the
+    #district (not state) and Aadhaar generated, separated by a tab.
+    #Skip rows without the correct number of tokens and also skip
+    #the header row.
+
+    #You can see a copy of the the input Aadhaar data
+    #in the link below:
+    #https://www.dropbox.com/s/vn8t4uulbsfmalo/aadhaar_data.csv
+
+    #Since you are printing the output of your program, printing a debug
+    #statement will interfere with the operation of the grader. Instead,
+    #use the logging module, which we've configured to log to a file printed
+    #when you click "Test Run". For example:
+    #logging.info("My debugging message")
+
+    #for line in sys.stdin:  # cycle through lines of code
+
+    # write to csv file, that will be used by the reducer
+    output_file = open("output.txt", "wb")
+
+    text_file = open("aadhaar_data.csv", "r")
+    rows = text_file.readlines()[1:]  # readlines and skip the header (1st line)
+
+    for row in rows:
+        data = row.strip().split(",")
+
+        # csv file has 12 header columns
+        # we only want rows with values in all header columns
+        if len(data) != 12:
+            continue
+
+        # print key-value pair
+        # index 3 = District
+        # index 8 = Aadhaar generated
+        print "{0}\t{1}".format(data[3], data[8])
+        output_file.write("{0}\t{1}".format(data[3], data[8]) + "\n")
+
+
 mapper()
--- intro_to_ds_programming_files/lesson_5/aadhaar_generated_quiz/aadhaar_generated_reducer.py
+++ intro_to_ds_programming_files/lesson_5/aadhaar_generated_quiz/aadhaar_generated_reducer.py
@@ -1,17 +1,42 @@
 import sys
-def reducer():
 
+
+def reducer():
+    # Also make sure to fill out the mapper code before clicking "Test Run" or "Submit".
+
+    # Each line will be a key-value pair separated by a tab character.
+    #Print out each key once, along with the total number of Aadhaar
+    #generated, separated by a tab. Make sure each key-value pair is
+    #formatted correctly! Here's a sample final key-value pair: 'Gujarat\t5.0'
+
+    #Since you are printing the output of your program, printing a debug
+    #statement will interfere with the operation of the grader. Instead,
+    #use the logging module, which we've configured to log to a file printed
+    #when you click "Test Run". For example:
+    #logging.info("My debugging message")
+
+    #for line in sys.stdin:
     aadhaar_generated = 0
     old_key = None
 
-    #Cycle through the list of key-value pairs emitted
-    #by your mapper, and print out each key once,
-    #along with the total number of Aadhaar generated,
-    #separated by a tab.  Assume that the list of key-
-    #value pairs will be ordered by key.  Make sure
-    #each key-value pair is formatted correctly!
-    #Here's a sample final key-value pair: "Gujarat\t5.0"
-    
-    # your code here
-    
+    text_file = open("output.txt", "r")
+    lines = text_file.readlines()
+    for line in lines:
+        data = line.strip().split("\t")
+
+        # each line must have a key-pair value
+        if len(data) != 2:
+            continue
+
+        if old_key and old_key != data[0]:
+            print "{0}\t{1}".format(old_key, aadhaar_generated)
+            aadhaar_generated = 0
+
+        aadhaar_generated += int(data[1])
+        old_key = data[0]
+
+    if old_key is not None:
+        print "{0}\t{1}".format(old_key, aadhaar_generated)
+
+
 reducer()
