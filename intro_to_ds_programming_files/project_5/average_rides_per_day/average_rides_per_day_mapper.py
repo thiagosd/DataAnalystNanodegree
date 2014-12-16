94bfca834ff1f9a8d3deac26ddd027ef97b33d02
Thiago D <thiagosd@gmail.com>
2014-12-15 21:47:37 -0500

    Project 5

--- intro_to_ds_programming_files/project_5/average_rides_per_day/average_rides_per_day_mapper.py
+++ intro_to_ds_programming_files/project_5/average_rides_per_day/average_rides_per_day_mapper.py
@@ -1,24 +1,56 @@
 import sys
 import string
 
-def mapper():
-    '''
-    In this exercise, we want to sum up all the values in the ENTRIEsn_hourly column in the 
-    turnstile_weather.csv. You can check out the csv file and its structure below:
-    https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv
-    
-    Each line of input will look like a row from our final Subway-MTA dataset, in csv format.
-    
-    This mapper should
-    1) return 0 as the key and
-    2) the number in the ENTRIEsn_hourly column as the value
-    3)  The key and the count should be separated by a tab
-        for example: 0\t12345
 
-    Example output to the reducer would look like this:
-    0   10501050105010.0
-    '''
-    for line in sys.stdin:
-    	# your code here
+def mapper():
+    """
+    The input to this mapper will be the final Subway-MTA dataset, the same as
+    in the previous exercise.  You can check out the csv and its structure below:
+    https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv
+
+    For each line of input, the mapper output should
+    1) return the UNIT as the key
+    2) return the number of ENTRIESn_hourly as the value
+    3) Separate the key and the value by a tab, for example:
+       R002\t105105.0
+
+    Since you are printing the actual output of your program, you
+    can't print a debug statement without breaking the grader.
+    Instead, you should use the logging module which we've configured
+    for you.
+
+    For example:
+    logging.info("My debugging message")
+
+    The logging module can be used to give you more control over your debugging
+    or other messages than you can get by printing them.  In this exercise, print
+    statements from your mapper will go to your reducer, and print statements
+    from your reducer will be considered your final output.  By contrast, messages
+    logged via the loggers we configured will be saved to two files, one
+    for the mapper and one for the reducer.  If you hit "Test Run", then we
+    will show the contents of those files once your program has finished running.
+    The logging module also has other capabilities; see
+    https://docs.python.org/2/library/logging.html for more information.
+    """
+
+    # write to csv file, that will be used by the reducer
+    output_file = open("output.txt", "wb")
+
+    text_file = open("turnstile_data_master_with_weather.csv", "r")
+    rows = text_file.readlines()[1:]  # readlines and skip the header (1st line)
+    for row in rows:
+        data = row.strip().split(",")
+
+        # csv file has 22 header columns
+        # we only want rows with values in all header columns
+        if len(data) != 22:
+            continue
+
+        # print key-value pair
+        # index 1 = UNIT
+        # index 6 = ENTRIESn_hourly
+        print "{0}\t{1}".format(data[1], data[6])
+        output_file.write("{0}\t{1}".format(data[1], data[6]) + "\n")
+
 
 mapper()
--- intro_to_ds_programming_files/project_5/average_rides_per_day/average_rides_per_day_reducer.py
+++ intro_to_ds_programming_files/project_5/average_rides_per_day/average_rides_per_day_reducer.py
@@ -1,20 +1,52 @@
 import sys
 
+
 def reducer():
     '''
-    Given the output of the mapper for this assignment, simply print
-    0 and then the average number of riders per day for the month of 05/2011,
-    separated by a tab.
-    
-    There are 31 days in 05/2011.
-    
-    Example output might look like this:
-    0   10501050.0
+    Given the output of the mapper for this exercise, the reducer should return
+    one row per unit, along with the total number of ENTRIESn_hourly over the
+    course of May (which is the duration of our data).
+
+    You can assume that the input to the reducer is sorted by UNIT, such that all rows
+    corresponding to a particular UNIT are grouped together.
+
+    The output should have a unit (the key) follow by a tab, follow by a value.
+    An example output row from the reducer might look like this:
+    R001\t500625.0
+
+    Since you are printing the actual output of your program, you
+    can't print a debug statement without breaking the grader.
+    Instead, you should use the logging module, which we've configured
+    to log to a file which will be printed when you hit "Test Run".
+    For example:
+    logging.info("My debugging message")
     '''
 
-    riders = 0
+    entries = 0
     old_key = None
 
-    # your code here
+    text_file = open("output.txt", "r")
+    lines = text_file.readlines()
+
+    for line in lines:
+        data = line.strip().split("\t")
+
+        # each line must have a key-pair value
+        if len(data) != 2:
+            continue
+
+        # we have a new key. reset the counter.
+        if old_key and old_key != data[0]:
+            print "{0}\t{1}".format(old_key, entries)
+            entries = 0
+
+        # same key. increment the counter. update the key
+        entries += float(data[1])
+        old_key = data[0]
+
+    # print value for last key
+    if old_key is not None:
+        print "{0}\t{1}".format(old_key, entries)
+
 
 reducer()
