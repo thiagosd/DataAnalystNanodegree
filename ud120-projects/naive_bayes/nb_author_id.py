edf8112a3fc4432aba5c389823d873a0c20b18fa
Thiago D <thiagosd@gmail.com>
2015-01-06 19:23:06 -0500

    Intro to Machine Learning - Lesson 1 Mini Project

--- ud120-projects/naive_bayes/nb_author_id.py
+++ ud120-projects/naive_bayes/nb_author_id.py
@@ -14,6 +14,7 @@
 import sys
 from time import time
 sys.path.append("../tools/")
+
 from email_preprocess import preprocess
 
 
@@ -23,10 +24,22 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+
+accuracy = accuracy_score(labels_test, pred)
+print accuracy
 
 
 #########################################################
