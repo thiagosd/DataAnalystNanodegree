3410b3bf855a9fd3e1d7bd0bf55a27e0aeffe0e6
Thiago D <thiagosd@gmail.com>
2015-01-28 23:05:27 -0500

    Intro to Machine Learning - Mini Project Lesson 13

--- ud120-projects/validation/validate_poi.py
+++ ud120-projects/validation/validate_poi.py
@@ -12,6 +12,7 @@
 
 import pickle
 import sys
+
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
@@ -24,7 +25,13 @@ data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
-
 ### it's all yours from here forward!  
+from sklearn import tree
+from sklearn import cross_validation
 
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X_train, y_train)
+print clf.score(X_test, y_test)
 
