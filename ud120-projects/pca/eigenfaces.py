94039f43e6ea80671b65e74c61e50dfcf895469f
Thiago D <thiagosd@gmail.com>
2015-01-28 19:49:36 -0500

    Intro to Machine Learning - Mini Project Lesson 12

--- ud120-projects/pca/eigenfaces.py
+++ ud120-projects/pca/eigenfaces.py
@@ -80,6 +80,14 @@ print "done in %0.3fs" % (time() - t0)
 
 eigenfaces = pca.components_.reshape((n_components, h, w))
 
+
+'''
+print Variance explained by 1st and 2nd Components
+print pca.explained_variance_ratio_[0]
+print pca.explained_variance_ratio_[1]
+'''
+
+
 print "Projecting the input data on the eigenfaces orthonormal basis"
 t0 = time()
 X_train_pca = pca.transform(X_train)
