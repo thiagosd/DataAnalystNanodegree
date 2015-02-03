636ea2167dde64f382aa02e3aa6f1ff254f74f29
Thiago D <thiagosd@gmail.com>
2015-02-02 21:22:58 -0500

    Intro to Machine Learning - Final Project - add plot

--- ud120-projects/final_project/poi_id.py
+++ ud120-projects/final_project/poi_id.py
@@ -2,20 +2,46 @@
 
 import sys
 import pickle
+import matplotlib.pyplot
+
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import test_classifier, dump_classifier_and_data
 
+# begin helper functions
+def plt_salary_bonus(data_dict):
+    """
+        plot Salary and Bonus
+    """
+    for point in data_dict:
+        salary = data_dict[point]['salary']
+        bonus = data_dict[point]['bonus']
+        matplotlib.pyplot.scatter(salary, bonus)
+
+    matplotlib.pyplot.xlabel("salary")
+    matplotlib.pyplot.ylabel("bonus")
+    matplotlib.pyplot.show()
+
+
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list = ['poi', 'salary']  # You will need to use more features
 
 ### Load the dictionary containing the dataset
-data_dict = pickle.load(open("final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("final_project_dataset.pkl", "r"))
 
 ### Task 2: Remove outliers
+# scatterplot bonus and salary to visually identify outliers
+plt_salary_bonus(data_dict)
+
+# clearly there is an outlier, remove it
+data_dict.pop('TOTAL', 0)
+# check for outliers again.
+# There are a few people with high salary or bonus, but we want to keep them because they are potential POI
+plt_salary_bonus(data_dict)
+
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
@@ -31,17 +57,18 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()    # Provided to give you a starting point. Try a varity of classifiers.
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+clf = GaussianNB()  # Provided to give you a starting point. Try avarietyy of classifiers.
+
+### Task 5: Tune your classifier to achieve better than .3 precision and recall
 ### using our testing script.
 ### Because of the small size of the dataset, the script uses stratified
-### shuffle split cross validation. For more info: 
+### shuffle split cross validation. For more info:
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 test_classifier(clf, my_dataset, features_list)
 
-### Dump your classifier, dataset, and features_list so 
+### Dump your classifier, dataset, and features_list so
 ### anyone can run/check your results.
 
 dump_classifier_and_data(clf, my_dataset, features_list)
