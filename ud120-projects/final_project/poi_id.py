9ef6f0f83d7f9f8fb01b1221ad2999b759093bb6
Thiago D <thiagosd@gmail.com>
2015-02-02 20:52:23 -0500

    Intro to Machine Learning - Final Project - get github updates

--- ud120-projects/final_project/poi_id.py
+++ ud120-projects/final_project/poi_id.py
@@ -1,81 +1,47 @@
 #!/usr/bin/python
 
-import matplotlib.pyplot as plt
 import sys
 import pickle
-import matplotlib
-import numpy as np
-
 sys.path.append("../tools/")
 
-from feature_format import featureFormat
-from feature_format import targetFeatureSplit
+from feature_format import featureFormat, targetFeatureSplit
+from tester import test_classifier, dump_classifier_and_data
 
-# begin helper functions
-def plotSalaryBonus(data_dict):
-    """
-        plot Salary and Bonus
-    """
-    for point in data_dict:
-        salary = data_dict[point]['salary']
-        bonus = data_dict[point]['bonus']
-        matplotlib.pyplot.scatter(salary, bonus)
+### Task 1: Select what features you'll use.
+### features_list is a list of strings, each of which is a feature name.
+### The first feature must be "poi".
+features_list = ['poi','salary'] # You will need to use more features
 
-    matplotlib.pyplot.xlabel("salary")
-    matplotlib.pyplot.ylabel("bonus")
-    matplotlib.pyplot.show()
+### Load the dictionary containing the dataset
+data_dict = pickle.load(open("final_project_dataset.pkl", "r") )
 
-# end helper functions
-
-### features_list is a list of strings, each of which is a feature name
-### first feature must be "poi", as this will be singled out as the label
-features_list = ['poi', 'salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus',
-                 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses',
-                 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 'director_fees',
-                 'to_messages', 'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi',
-                 'shared_receipt_with_poi']
-
-
-
-### load the dictionary containing the dataset
-data_dict = pickle.load(open("final_project_dataset.pkl", "r"))
-
-### we suggest removing any outliers before proceeding further
-# scatterplot bonus and salary to visually identify outliers
-plotSalaryBonus(data_dict)
-
-# clearly there is an outlier, remove it
-data_dict.pop('TOTAL', 0)
-# check for outliers again. There are a few people with high salary or bonus, but we want to keep them because they are potential POI
-plotSalaryBonus(data_dict)
-
-### if you are creating any new features, you might want to do that here
-### store to my_dataset for easy export below
+### Task 2: Remove outliers
+### Task 3: Create new feature(s)
+### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
-
-### these two lines extract the features specified in features_list
-### and extract them from data_dict, returning a numpy array
+### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list)
-
-### if you are creating new features, could also do that here
-
-
-
-### split into labels and features (this line assumes that the first
-### feature in the array is the label, which is why "poi" must always
-### be first in features_list
 labels, features = targetFeatureSplit(data)
 
+### Task 4: Try a varity of classifiers
+### Please name your classifier clf for easy export below.
+### Note that if you want to do PCA or other multi-stage operations,
+### you'll need to use Pipelines. For more info:
+### http://scikit-learn.org/stable/modules/pipeline.html
 
-### machine learning goes here!
-### please name your classifier clf for easy export below
-from sklearn import tree
-clf = None
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()    # Provided to give you a starting point. Try a varity of classifiers.
 
+### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+### using our testing script.
+### Because of the small size of the dataset, the script uses stratified
+### shuffle split cross validation. For more info: 
+### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
-### dump your classifier, dataset and features_list so 
-### anyone can run/check your results
-pickle.dump(clf, open("my_classifier.pkl", "w"))
-pickle.dump(data_dict, open("my_dataset.pkl", "w"))
-pickle.dump(features_list, open("my_feature_list.pkl", "w"))
+test_classifier(clf, my_dataset, features_list)
+
+### Dump your classifier, dataset, and features_list so 
+### anyone can run/check your results.
+
+dump_classifier_and_data(clf, my_dataset, features_list)
